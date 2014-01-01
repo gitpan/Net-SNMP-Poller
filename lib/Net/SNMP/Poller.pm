2977b9633e44d9112945190b784e24a84959d529
Satoshi Yagi <syagi@cpan.org>
2014-01-01 04:31:07 +0000

    Import of SYAGI/Net-SNMP-Poller-0.05 from CPAN.
    gitpan-cpan-distribution: Net-SNMP-Poller
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         SYAGI/Net-SNMP-Poller-0.05.tar.gz
    gitpan-cpan-author:       SYAGI
    gitpan-cpan-maturity:     released

--- lib/Net/SNMP/Poller.pm
+++ lib/Net/SNMP/Poller.pm
@@ -12,11 +12,11 @@ Net::SNMP::Poller - Simple poller for non-blocking SNMP queries
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 
 =head1 SYNOPSIS
